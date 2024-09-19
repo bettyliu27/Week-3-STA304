49e1f8fd0b9f80d69a8254cd85abcf0a3c55679d
bettyliu27 <98493245+bettyliu27@users.noreply.github.com>
2024-09-19 18:34:26 -0400

    Add files via upload

---
+++ scripts/05-replications.R
@@ -0,0 +1,18 @@
+#### Preamble ####
+# Purpose: Replicated graphs from... [...UPDATE THIS...]
+# Author: Joyce Lin, Aliza Mithwani, Betty Liu, Manjun Zhu
+# Date: 19 September 2024
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Workspace setup ####
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Load data ####
+# [...ADD CODE HERE...]
+
+
+
